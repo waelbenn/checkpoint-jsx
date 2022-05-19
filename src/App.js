94fa7e1ac9e607a957093c9afc869c28fe262154
waelbenn <waelbenngx@outlook.fr>
2022-05-19 09:40:58 -0800

    jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import './style.css';
+import imageInSrc from "./imageInSrc.jpg"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{border:"solid 1px black",maxWidth:'100vw'}}>
+
+<h1 className='title red'>Wael ben abdallah</h1>
+
+<br/>
+
+<img src={imageInSrc} alt='road'/>
+
+<br/>
+
+<img src="/imageInPublic.jpg" alt='river'/>
+
+</div>
+
+<video width="320" height="240" controls>
+
+<source src="myVideo.mp4" type="video/mp4"/>
+
+</video>
     </div>
   );
 }
